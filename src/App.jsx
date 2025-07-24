3afcc35ab2ba226f1e0589467d2b560cb4706953
vandeson2 <vandeson2@gmail.com>
2025-07-24 17:03:22 +0200

    Creado el componente de selector de servicios

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ServiceSelector from './components/ServiceSelector'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedService, setSelectedService] = useState(null)
 
   return (
-    <>
+    
+ <div>
+     {!selectedService ? (
+       <ServiceSelector onSelect={setSelectedService} />
+     ) :(
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h2>Has selecionado: {selectedService.name}</h2>
+        <button onClick={() => setSelectedService(null)}>
+          Volver
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     )}
+ </div>
   )
 }
 
---
+++ src/components/ServiceSelector.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const services = [
+    {id: 1, name: 'Body Pump'},
+    {id: 2, name: 'HIIT'},
+    {id: 3, name: 'Yoga'},
+    {id: 4, name: 'Pilates'},
+    {id: 5, name: 'Spinning'},
+    {id: 6, name: 'Zumba'}, 
+    {id: 7, name: 'CrossFit'},
+]
+
+export default function serviceSelector({onSelect}){
+    return(
+        <div className="p-4">
+            <h2 className="text-xl font-semibold mb-4 text-center">
+                ¿Qué servicio deseas reservar?
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5">
+                {services.map((service) => (
+                    <button
+                        key={service.id}
+                        onClick={() => onSelect(service)}
+                        className="bg-white shadow rounded-lg p-4 text-left hover:bg-green-100 transition"
+                    >
+                        <h3 className="text-lg font-bold">{service.name}</h3>
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
+
