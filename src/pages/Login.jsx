7075f31f5cb7152b560a65f86bd23d4aaf665b44
vandeson2 <vandeson2@gmail.com>
2025-08-23 19:59:11 +0200

    Instalado Tailwindcss colocación de div de login y home

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,33 +48,47 @@ export default function Login(){
     };
 
     return (
-        <div>
-            <h2>Iniciar Sesión</h2>
-    
-            <form onSubmit={handleEmailLogin}>
-                <input 
-                    type="email" 
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <input 
-                    type="password"
-                    value={password}
-                    onChange={(e) =>setPassword(e.target.value)} 
-                    required
-                />
-                {error && <p>{error}</p>}
+        <div className="flex justify-center items-center min-h-screen bg-gray-100 px-2">
+            <div className="flex flex-col items-center p-6 sm:p-10 rounded-lg shadow-2xl w-full max-w-md">
+                <h2 className="font-bold text-2xl mb-5 text-center">Iniciar Sesión</h2>
+        
+                <form 
+                    onSubmit={handleEmailLogin} 
+                    className="flex flex-col w-full gap-4"
+                >
+                    <label htmlFor="email">Correo</label>
+                    <input 
+                        type="email" 
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        className="input-field"
+                    />
 
-                <button type="submit">
-                    Iniciar sesión
+                    <label htmlFor="password">Contraseña</label>
+                    <input 
+                        type="password"
+                        value={password}
+                        onChange={(e) =>setPassword(e.target.value)} 
+                        required
+                        className="input-field"
+                    />
+                    {error && <p>{error}</p>}
+
+                    <button type="submit" className="btn w-full bg-blue-500 text-white  hover:bg-blue-600 transition-colors">
+                        Iniciar sesión
+                    </button>
+                </form>
+
+                <hr className="w-full my-6 border-t-2  border-gray-400"/>
+
+                <button 
+                    onClick={handleGoogleLogin}
+                    className="btn border w-full"
+                >
+                    Iniciar con Google
                 </button>
-            </form>
-
-            <hr />
-            <button onClick={handleGoogleLogin}>
-                Iniciar con Google
-            </button>
+            </div>
         </div>
     );
 };
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -3,18 +3,29 @@ import { useNavigate } from "react-router-dom";
 export default function Home(){
     const navigate = useNavigate();
     return (
-        <div>
-            <h1>Bienvenido a Estudios Vitalis</h1>
-            <p>
-                Conecta con tu bienestar a través del yoga, el masaje terapéutico y la energía del reiki.
-                Reserva tu sesión fácilmente desde aquí.
-            </p>
-            <button onClick={() => navigate("/reservas")}>
-                Reservar ahora
-            </button>
-            <button onClick={() => navigate("/my-reservation")}>
-                Mis Reservas
-            </button>    
+        <div className="flex flex-col md:flex-row-reverse min-h-screen  bg-gray-100">
+
+             <div className="w-full h-32 md:h-auto  bg-yellow-500">
+
+            </div>
+            <div className="flex flex-col  p-3 sm:p-10 w-full sm:max-w-lg bg-white mx-auto">
+                <h1 className="text-3xl sm:text-5xl font-bold mb-4 ">Bienvenido a Estudios Vitalis</h1>
+                <p className="text-base sm:text-lg mb-8">
+                    Conecta con tu bienestar a través del yoga, el masaje terapéutico y la energía del reiki.
+                    Reserva tu sesión fácilmente desde aquí.
+                </p>
+                <button onClick={() => navigate("/reservas")}
+                        className="btn border w-full sm:w-2/3 mb-3"
+                >
+                    Reservar ahora
+                </button>
+                <button onClick={() => navigate("/my-reservation")}
+                        className="btn border w-full sm:w-2/3 mb-3"
+                >
+                    Mis Reservas
+                </button>   
+            </div> 
+           
         </div>
     );
 };
